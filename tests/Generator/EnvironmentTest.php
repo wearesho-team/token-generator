011e5a5f646c66052b775ee6f9030e4859a4eb3f
Alexander Yagich <aleksa.yagich@gmail.com>
2019-02-18 12:37:04 +0200

    Implement default generator for environment generator (#3)

--- tests/Generator/EnvironmentTest.php
+++ tests/Generator/EnvironmentTest.php
@@ -2,8 +2,9 @@
 
 namespace Wearesho\Token\Tests\Generator;
 
+use Horat1us\Environment\Exception\Missing;
 use PHPUnit\Framework\TestCase;
-use Wearesho\Token\Generator\Environment;
+use Wearesho\Token\Generator;
 
 /**
  * Class EnvironmentTest
@@ -13,8 +14,22 @@ class EnvironmentTest extends TestCase
 {
     public function testGenerate(): void
     {
-        $generator = new Environment(5);
+        $generator = new Generator\Environment(5);
         putenv('TOKEN_GENERATOR_VALUE=g');
         $this->assertEquals('ggggg', $generator->generate());
+    }
+
+    public function testDefault(): void
+    {
+        $generator = new Generator\Environment(3, 'INVALID_ENV_KEY_', new Generator\Char(5, ['a']));
+        $this->assertEquals('aaaaa', $generator->generate());
+    }
+
+    public function testMissingAll(): void
+    {
+        $generator = new Generator\Environment(3, 'INVALID_KEY_');
+        $this->expectException(Missing::class);
+        $this->expectExceptionMessage('Missing environment key INVALID_KEY_VALUE');
+        $generator->generate();
     }
 }
